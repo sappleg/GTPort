5c02bba21c16dca38576430537eebdb1296291f3
Jonathan Porter <jon.porter31@gmail.com>
2012-12-02 15:05:03 -0500

    updated faculty model

---
+++ src/models/Faculty.py
@@ -0,0 +1,10 @@
+class Faculty:
+    def __init__(self):
+        self.username = ""
+
+    def setUsername(self, un):
+        self.username = un
+
+if __name__=="__main__":
+    print(self.username)
+    
