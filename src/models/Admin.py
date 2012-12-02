789e1b248869be5af37bcdea9af72d3a5dc26b02
Jonathan Porter <jon.porter31@gmail.com>
2012-12-02 15:08:56 -0500

    updated faculty and admin models

--- src/models/Admin.py
+++ src/models/Admin.py
@@ -5,5 +5,8 @@ class Admin:
     def setUsername(self, un):
         self.username = un
 
+    def getUsername(self):
+        return self.username
+
 if __name__="__main__":
     print(self.username)
--- src/models/Faculty.py
+++ src/models/Faculty.py
@@ -5,6 +5,9 @@ class Faculty:
     def setUsername(self, un):
         self.username = un
 
+    def getUsername(self):
+        return self.username
+
 if __name__=="__main__":
     print(self.username)
     
